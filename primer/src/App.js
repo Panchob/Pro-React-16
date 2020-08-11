b219a9e1be67c20c0b4191b3ab0a7d5475cb181d
Francis <bilodeaurobillardfrancis@gmail.com>
2020-08-11 16:42:39 -0400

    Feat: chapter 3 code

---
+++ primer/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 4
+    }
+  }
+
+  isEven(val) {
+    return val %2 === 0
+  }
+
+  getClassName(val) {
+    return this.isEven(val) ? "bg-primary text-white text-center p-2 m-2" : "bg-secondary text-white text-center p-2 m-2"
+  }
+
+  handleClick = () => this.setState({
+    count: this.state.count + 1
+  });
+
+  render = () =>
+  <div className="container-fluid p-4">
+    <div className="row bg-info text-white p-2">
+      <div className="col font-weight-bold">Value</div>
+      <div className="col-6 font-weight-bold">Even?</div>
+    </div>
+    <div className="row bg-light p-2 border">
+      <div className="col">{ this.state.count}</div>
+      <div className="col-6">{ this.isEven(this.state.count) ? "Even": "Odd" }</div>
+    </div>
+    <div className="row>">
+      <div className="col">
+        <button className="btn btn-info m-2" onClick={ this.handleClick }>
+          Click Me 
+        </button>
+      </div>
+    </div>
+  </div>
+}
