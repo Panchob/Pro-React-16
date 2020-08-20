d083db574aa6da42f96c23d85d0a5a46ad93898b
Francis <bilodeaurobillardfrancis@gmail.com>
2020-08-20 08:24:33 -0400

    Feat: config for graphql integration

--- sportsstore/data.js
+++ sportsstore/data.js
@@ -1,24 +1,61 @@
 const faker = require("faker");
-const data = []
-const categories = ["Watersports", "Soccer", "Chess", "Running"];
 faker.seed(100);
+const categories = ["Watersports", "Soccer", "Chess"];
+const products = []
 
 for (let i = 1; i <= 503; i++) {
     const category = faker.helpers.randomize(categories);
-    data.push({
-        id: i,
-        name: faker.commerce.productName(),
+
+    products.push({
+        id: 1,
+        name: faker.commerce.productName,
         category: category,
         description: `${category}: ${faker.lorem.sentence(3)}`,
         price: Number(faker.commerce.price())
-
     })
 }
 
-module.exports = function () {
-    return {
-        categories: categories,
-        products: data,
-        orders: []
+const orders = []
+
+for (let i = 1; i <= 103; i++) {
+    const fname = faker.name.firstName();
+    const lname = faker.name.lastName();
+    const order = {
+        id: i,
+        name: `${fname} ${lname}`,
+        email: faker.internet.email(fname, lname),
+        address: faker.address.streetAddress(),
+        city: faker.address.city(),
+        zip: faker.address.zipCode(),
+        country: faker.address.country(),
+        shipped: faker.random.boolean(),
+        products:[]
     }
+
+    const productCount = faker.random.number({min: 1, max: 5});
+    const product_ids = []
+    
+    while (product_ids.length < productCount) {
+        const candidateId = faker.random.number({ min:1, max:products.length });
+
+        if (product_ids.indexOf(candidateId) === -1) {
+            product_ids.push(candidateId);
+        }
+
+        for (let j = 0; j < productCount; j++) {
+            order.products.push({
+                quantity: faker.random.number({min:1, max:10}),
+                product_id: product_ids[j]
+            })
+        }
+
+    }
+
+    orders.push(order)
 }
+
+module.exports = () => ({
+    categories,
+    products,
+    orders
+})
